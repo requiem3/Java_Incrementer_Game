068c3d132645ae0e35bb99d4aefb9f746cca0fa3
Kyle Heitman <kyle.heitman@uconn.edu>
2015-06-19 06:07:12 -0400

    Finished threading for game class
    Just implemented the threading for the game class and everything is
    working as intended. Next I will do the massive refactoring overhaul
    which will probably take a little bit. I am going to take a break and
    nap for a bit before I get started.

--- test_es/src/test_es/Game.java
+++ test_es/src/test_es/Game.java
@@ -2,13 +2,31 @@ package test_es;
 
 import java.awt.Container;
 
-public class Game {
+import javax.swing.JLabel;
+
+import systems.*;
+
+public class Game implements Runnable {
+	//Game variables
+	private int FPS = 60;
 	
-	public Game(Container pane) {
+	//Systems
+	private NumberSystem numSys;
+	
+	//GUI Variables
+	private JLabel levelLabel;
+	private JLabel expLabel;
+	private JLabel cashLabel;
+	
+	public Game(Board board) {
+		levelLabel = board.getLevelLabel();
+		expLabel = board.getExpLabel();
+		cashLabel = board.getCashLabel();
 		
+		numSys = new NumberSystem();
 	}
 	
-	public void start() {
-		
+	public void run() {
+		return(0);
 	}
 }
